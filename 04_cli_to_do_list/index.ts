42e03b16e8d3cd11a9734533d7d460e83db86f46
Samia72329 <samia72329salman@gmail.com>
2024-03-31 00:17:42 +0500

    pushed node project 4 cli based to do list application

---
+++ 04_cli_to_do_list/index.ts
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const viewList = (): void => {
+    mylist.forEach((element, index) => {
+        console.log(`Task ${index + 1}: ${element}`);
+    })
+}
+const remove = (index: number): void => {
+    mylist.splice(index - 1, 1);
+}
+
+let mylist: any[] = [];
+console.log(mylist.length);
+let flag = true;
+while (flag) {
+    let actionqs = await inquirer.prompt
+        (
+            [
+                {
+                    name: "action",
+                    type: "list",
+                    message: "Select anyone",
+                    choices: ["Add a new task", "Delete a task", "View To-do-List", "Exit"]
+                },
+
+            ]
+        )
+
+    switch (actionqs.action) {
+        case "Add a new task":
+            let taskqs = await inquirer.prompt
+                (
+                    [
+                        {
+                            name: "task",
+                            type: "input",
+                            message: "Add task:",
+                        },
+
+                    ]
+                )
+            mylist.push(taskqs.task);
+            break;
+        case "Delete a task":
+            let removeqs = await inquirer.prompt
+                (
+                    [
+                        {
+                            name: "remove",
+                            type: "input",
+                            message: "Enter task number to remove:",
+                        },
+
+                    ]
+                )
+            if (removeqs.remove - 1 < 0) {
+                console.log("invalid task number");
+            }
+            
+            else if (removeqs.remove > mylist.length) {
+                console.log("invalid task number");
+            }
+            else {
+                remove(removeqs.remove); // removes task at the specified index
+            }
+            break;
+        case "View To-do-List":
+            if (mylist.length == 0) {
+                console.log("Empty List")
+            }
+            else { viewList(); }
+            break;
+        case "Exit":
+            flag = false;
+            break;
+
+    }
+}
+console.log("--- Bye! See you again! ---");
