c7ea7359e23dbb2a58009e357c523848fa15929a
Samia72329 <samia72329salman@gmail.com>
2024-04-03 23:41:56 +0500

    Improved CLI UI by using npm chalk package

--- 02_guessTheNumber/index.js
+++ 02_guessTheNumber/index.js
@@ -1,9 +1,29 @@
 #! /usr/bin/env node
 /* write a program to guess the number betweeen 1 -100 */
-console.log("Welcome to Guess The Hidden Number Game!");
-console.log("Game Instructions:");
-console.log("*You have to guess a number between 0 to 100\n*You have 100 attempts\n*Score will be calculated based on the number of attempts made.\n*The higher the attempts the lesser the score\n");
 import inquirer from 'inquirer';
+import chalk from 'chalk';
+const pink = chalk.hex('#e647db');
+const green = chalk.hex('#2be35c');
+const yellow = chalk.hex('#f0f033');
+console.log(pink(`
+
+██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗ 
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝                                                             
+`));
+console.log(green(`      
+_____                        _____  _           _____  _    _    _              _____              _             
+|   __| _ _  ___  ___  ___   |_   _|| |_  ___   |  |  ||_| _| | _| | ___  ___   |   | | _ _  _____ | |_  ___  ___ 
+|  |  || | || -_||_ -||_ -|    | |  |   || -_|  |     || || . || . || -_||   |  | | | || | ||     || . || -_||  _|
+|_____||___||___||___||___|    |_|  |_|_||___|  |__|__||_||___||___||___||_|_|  |_|___||___||_|_|_||___||___||_|  
+                                                                                                                  
+
+`));
+console.log(green("Game Instructions:"));
+console.log(green(("*You have to guess a number between 0 to 100\n*You have 100 attempts\n*Score will be calculated based on the number of attempts made.\n*The higher the attempts the lesser the score\n")));
 let hiddenNum = Math.round(Math.random() * 100);
 // console.log(randomNum);
 let chance = 100;
@@ -12,21 +32,29 @@ do {
     answer = await inquirer.prompt([
         { name: "userInput",
             type: "number",
-            message: "Guess the hidden number:", }
+            message: pink("Guess the hidden number:"), }
     ]);
     console.log(answer.userInput);
     chance--;
     if (answer.userInput === hiddenNum) {
-        console.log("You guessed it right! :)");
+        console.log(green(`                    
+                                        __   
+      __ __              _ _ _  _      |  |  
+     |  |  | ___  _ _   | | | ||_| ___ |  |  
+     |_   _|| . || | |  | | | || ||   ||__|  
+       |_|  |___||___|  |_____||_||_|_||__|  
+                                             
+     
+      `));
         let score = chance + 1;
-        console.log("Your score is: " + score);
+        console.log(green("Your score is: ") + score);
         break;
     }
     else if (answer.userInput < hiddenNum) {
-        console.log(`the hidden number is greater than guessed number :(\ntry again!!!\nchances left: ${chance}`);
+        console.log(green(`the hidden number is greater than guessed number :(\ntry again!!!\nchances left: ${chance}`));
     }
     else if (answer.userInput > hiddenNum) {
-        console.log(`the hidden number is less than guessed number :(\ntry again!!!\nchances left: ${chance}`);
+        console.log(green(`the hidden number is less than guessed number :(\ntry again!!!\nchances left: ${chance}`));
     }
 } while (chance >= 1);
-console.log(`the hidden number was ${hiddenNum}`);
+console.log(pink(`the hidden number was ${hiddenNum}`));
