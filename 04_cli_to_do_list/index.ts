e1323c913fb843fa3f18209392a9ddc120e5c950
Samia72329 <samia72329salman@gmail.com>
2024-04-06 12:50:09 +0500

    fixed bug in delete task operation, improved CLI UI

--- 04_cli_to_do_list/index.ts
+++ 04_cli_to_do_list/index.ts
@@ -1,80 +1,128 @@
 #! /usr/bin/env node
 import inquirer from "inquirer"
-
-const viewList = (): void => {
-    mylist.forEach((element, index) => {
-        console.log(`Task ${index + 1}: ${element}`);
-    })
+import chalk from "chalk"
+const green = chalk.bold.bgHex("#041400").hex("#27ff0a")
+const red = chalk.bold.bgHex("#0f0000").hex("#ff0505")
+const yel = chalk.hex("#fff705")
+const cyan = chalk.hex("#1afff7")
+const pink = chalk.hex("#f50f73")
+let myTask: any[] = [];
+// function definition
+const viewTask = (): void => {
+    if (myTask.length == 0) {
+        console.log(cyan(`
+        +-++-++-++-++-+ +-++-++-++-+
+        |e||m||p||t||y| |l||i||s||t|
+        +-++-++-++-++-+ +-++-++-++-+
+        `))
+    } else {
+        console.log(pink(`
+        +-++-+ +-++-+ +-++-++-++-+
+        |t||o| |d||o| |l||i||s||t|
+        +-++-+ +-++-+ +-++-++-++-+
+        `))
+        myTask.forEach((element, index) => {
+            console.log(pink(`          Task ${index + 1}: ${element}`));
+        })
+    }
 }
-const remove = (index: number): void => {
-    mylist.splice(index - 1, 1);
+
+const remove = async () => {
+    if(myTask.length == 0){
+        console.log(cyan(`
+        +-++-++-++-++-++-++-+ +-++-+ +-++-++-++-++-++-+
+        |n||o||t||h||i||n||g| |t||o| |d||e||l||e||t||e|
+        +-++-++-++-++-++-++-+ +-++-+ +-++-++-++-++-++-+
+        `))
+    }
+    else {
+    await inquirer.prompt
+        (
+            [
+                {
+                    name: "delTask",
+                    type: "list",
+                    message: yel("Select task to remove"),
+                    choices: myTask,
+                },
+
+            ]
+        ).then((answer) => {
+            
+            
+            let remTask = myTask.splice(myTask.indexOf(answer.delTask), 1); // removes task at the specified index
+            console.log(red(`     Succesfully removed: "${remTask}" from your to-do list      `));
+
+        }
+        )
+}
+}
+const addTask = async () => {
+    await inquirer.prompt
+        (
+            [
+                {
+                    name: "add",
+                    type: "input",
+                    message: yel("Add task:"),
+                },
+
+            ]
+        ).then((answer) => {
+
+            if (answer.add != ""){
+                let len  = myTask.push(answer.add);
+                console.log(green(`     Succesfully added: "${myTask[len-1]}" in your to-do list      `));
+            }
+            else
+                console.log("Task cannot be empty");
+        })
+
 }
 
-let mylist: any[] = [];
-console.log(mylist.length);
-let flag = true;
-while (flag) {
-    let actionqs = await inquirer.prompt
+
+const toDoList = async () => {
+    await inquirer.prompt
         (
             [
                 {
                     name: "action",
                     type: "list",
-                    message: "Select anyone",
-                    choices: ["Add a new task", "Delete a task", "View To-do-List", "Exit"]
+                    message: yel('|S||E||L||E||C||T|'),
+                    // choices: [pink("Add task"), pink("Delete task"), pink("View tasks"), pink("Exit")]
+                    choices: ["Add task", "Delete task", "View tasks", "Exit"]
                 },
 
             ]
+        ).then(async (answer) => {
+            switch (answer.action) {
+                case "Add task":
+                    await addTask();
+                    break;
+                case "Delete task":
+                    await remove();
+                    break;
+                case "View tasks":
+                    viewTask();
+                    break;
+                case "Exit":
+                    flag = false;
+                    break;
+
+            }
+        }
         )
-
-    switch (actionqs.action) {
-        case "Add a new task":
-            let taskqs = await inquirer.prompt
-                (
-                    [
-                        {
-                            name: "task",
-                            type: "input",
-                            message: "Add task:",
-                        },
-
-                    ]
-                )
-            mylist.push(taskqs.task);
-            break;
-        case "Delete a task":
-            let removeqs = await inquirer.prompt
-                (
-                    [
-                        {
-                            name: "remove",
-                            type: "input",
-                            message: "Enter task number to remove:",
-                        },
-
-                    ]
-                )
-            if (removeqs.remove - 1 < 0) {
-                console.log("invalid task number");
-            }
-            
-            else if (removeqs.remove > mylist.length) {
-                console.log("invalid task number");
-            }
-            else {
-                remove(removeqs.remove); // removes task at the specified index
-            }
-            break;
-        case "View To-do-List":
-            if (mylist.length == 0) {
-                console.log("Empty List")
-            }
-            else { viewList(); }
-            break;
-        case "Exit":
-            flag = false;
-            break;
-
-    }
 }
+
+console.log(green(`
+.########..#######.....########...#######.....##.......####..######..########
+....##....##.....##....##.....##.##.....##....##........##..##....##....##...
+....##....##.....##....##.....##.##.....##....##........##..##..........##...
+....##....##.....##....##.....##.##.....##....##........##...######.....##...
+....##....##.....##....##.....##.##.....##....##........##........##....##...
+....##....##.....##....##.....##.##.....##....##........##..##....##....##...
+....##.....#######.....########...#######.....########.####..######.....##...
+`))
+let flag = true;
+while (flag) { await toDoList(); }
 console.log("--- Bye! See you again! ---");
